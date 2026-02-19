160cc6609b73396c21abeff34d171eef5cf5c628
whohensee <106775295+whohensee@users.noreply.github.com>
2026-02-19 12:12:22 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal Site
+
+This page is designed to succinctly provide professional information to interested professors, prospective employers, etc in a format which offers important details immediately and follow-up information on-demand.
+
+As such, I want the initial appearance of the site to be resume-like, but interactable to provide more information.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let toggled = $state(false);
+
+	function swapToggled() {
+		toggled = !toggled;
+	}
+</script>
+
+<button type="button" onclick={swapToggled}>
+	<p>
+		<strong>Bachelor of Arts, Astronomy</strong> from UC Berkeley
+		{#if !toggled}
+			◁
+		{:else}
+			▽
+		{/if}
+	</p>
+	{#if toggled}
+		<p>Toggled is on!</p>
+	{/if}
+</button>
