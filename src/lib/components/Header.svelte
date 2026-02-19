11e302d8a066bee4f77d15ae5a326b1dcb37ad6b
whohensee <106775295+whohensee@users.noreply.github.com>
2026-02-19 12:28:14 -0800

    Introduce basic file and page structure

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="flex w-fit flex-col text-center">
+	<h1>William Hohensee</h1>
+	<h2>Software Developer, Astrophysicist</h2>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+
 	let toggled = $state(false);
 
 	function swapToggled() {
@@ -6,16 +8,20 @@
 	}
 </script>
 
-<button type="button" onclick={swapToggled}>
-	<p>
-		<strong>Bachelor of Arts, Astronomy</strong> from UC Berkeley
-		{#if !toggled}
-			◁
-		{:else}
-			▽
+<div class="flex flex-col items-center">
+	<Header />
+
+	<button type="button" onclick={swapToggled}>
+		<p>
+			<strong>Bachelor of Arts, Astronomy</strong> from UC Berkeley
+			{#if !toggled}
+				◁
+			{:else}
+				▽
+			{/if}
+		</p>
+		{#if toggled}
+			<p>Toggled is on!</p>
 		{/if}
-	</p>
-	{#if toggled}
-		<p>Toggled is on!</p>
-	{/if}
-</button>
+	</button>
+</div>
