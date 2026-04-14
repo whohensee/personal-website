ec88ec98ee6132a06032d5cfad1fa563f14f75c9
whohensee <106775295+whohensee@users.noreply.github.com>
2026-04-14 10:29:33 -0700

    Add github link to CSE

--- src/lib/components/work_showcase/my_work/WorkCSE.svelte
+++ src/lib/components/work_showcase/my_work/WorkCSE.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import ProjectCard from '../ProjectCard.svelte';
-	import type { validPaths } from '$lib/types';
 
 	const project_title = 'Image Processing Pipeline Development';
 	const project_jobname = 'Computer Systems Engineer at Lawrence Berkeley National Lab';
 	const project_description =
 		'Software Development in Python and Javascript, creating a data-processing pipeline called SeeChange which turns raw telescope images into lightcurves and associated scientific data products. This work involved extensive test-driven development in Python, creation of a controller web-application with Flask and Javascript, and management of the PostgreSQL database and associated cloud archive. SeeChange was also containerized for deployment onto cloud services, utilizing both Docker and Podman when appropriate.';
 	const project_date = 'Sept 2022 - October 2025';
-	const has_link = false;
+	const has_link = true;
 
-	const project_link: validPaths = '/';
-	const link_text = 'Detailed Project Case Study';
+	const project_link = 'https://github.com/c3-time-domain/SeeChange';
+	const link_text = 'Project Github';
 
 	const has_github = false;
 	const github_link = '';
